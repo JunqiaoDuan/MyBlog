ec4dcd949cdc156c06478358195778768ebb5c8e
Junqiao <junqiaoduan@gmail.com>
2025-06-03 13:51:06 +0800

    add heatmap

---
+++ src/MyBlog.Service.Shared/Dtos/ExternalDto/Dida365/Dida365PomodoroHistoryDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.Service.Shared.Dtos.ExternalDto.Dida365
+{
+    public class Dida365PomodoroHistoryDto
+    {
+        public int Duration { get; set; }
+        public string Day { get; set; } = string.Empty;
+        public string Timezone { get; set; } = string.Empty;
+    }
+}
---
+++ src/MyBlog.Service.Shared/Dtos/VisualDto/DateIntValue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.Service.Shared.Dtos.VisualDto
+{
+    public class DateIntValue
+    {
+        public string Date { get; set; }
+        public int Value { get; set; }
+    }
+}
