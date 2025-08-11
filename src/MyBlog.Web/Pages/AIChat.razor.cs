0fb8bc3ad602e00545e72c52a8693ab5757b23c5
Junqiao <junqiaoduan@gmail.com>
2025-08-11 16:47:01 +0800

    integrate MudBlazor

--- src/MyBlog.Web/Pages/AIChat.razor.cs
+++ src/MyBlog.Web/Pages/AIChat.razor.cs
@@ -1,6 +1,16 @@
-﻿namespace MyBlog.Web.Pages
+﻿using MudBlazor;
+
+namespace MyBlog.Web.Pages
 {
     public partial class AIChat
     {
+        private string _enteredMessage { get; set; } = "";
+        private MudTextField<string> multilineReference;
+
+        private void SendMessage()
+        {
+            // Add logic to send message and update chat history
+            _enteredMessage = "";
+        }
     }
 }
