8978aa87d71ade42410f9387596ab47c97bea78e
Junqiao <junqiaoduan@gmail.com>
2025-08-11 15:31:35 +0800

    clean code

---
+++ src/MyBlog.Web/Shared/NavMenu.razor.cs
@@ -0,0 +1,29 @@
+﻿using MyBlog.Service.Shared.Dtos.Queries;
+
+namespace MyBlog.Web.Shared
+{
+    public partial class NavMenu
+    {
+        private ProfileQueryView? blogOwner;
+        private bool hideChatBadge = false;
+
+        protected override async Task OnInitializedAsync()
+        {
+            var profiles = await _profileService.GetProfilesAsync();
+            blogOwner = profiles.FirstOrDefault(p => p.Name == "Name");
+        }
+
+        private async void OnMenuClick(string menuCode)
+        {
+            if (menuCode == "chat")
+            {
+                hideChatBadge = true;
+                StateHasChanged();
+            }
+
+            var queueClient = _queueService.GetQueueClient("click-menu");
+            await queueClient.CreateIfNotExistsAsync();
+            await queueClient.SendMessageAsync($"Clicked {menuCode}");
+        }
+    }
+}
