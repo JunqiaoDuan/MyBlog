3c337304fa58d873e1fff98d36dacddf2fabafa4
Junqiao <junqiaoduan@gmail.com>
2025-08-12 11:33:16 +0800

    optimize chat box style

--- src/MyBlog.Web/Pages/AIChat.razor.cs
+++ src/MyBlog.Web/Pages/AIChat.razor.cs
@@ -5,7 +5,12 @@ namespace MyBlog.Web.Pages
     public partial class AIChat
     {
         private string _enteredMessage { get; set; } = "";
-        private MudTextField<string> multilineReference;
+        private string _selectedModel { get; set; } = "model1";
+        private List<KeyValuePair<string, string>> _models = [
+            new KeyValuePair<string, string>("model1", "model1"),
+            new KeyValuePair<string, string>("model2", "model2"),
+            new KeyValuePair<string, string>("model3sdfdsfsdf", "model3sdfdsfsdf"),
+        ];
 
         private void SendMessage()
         {
